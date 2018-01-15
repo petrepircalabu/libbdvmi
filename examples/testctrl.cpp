7ff9ed64db828c7f24a0ca7402461b4611a89fe3
Petre Pircalabu <ppircalabu@bitdefender.com>
2018-01-15 13:42:39 +0200

    SQUASHME: update test to unpause domain
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

--- examples/testctrl.cpp
+++ examples/testctrl.cpp
@@ -1,10 +1,13 @@
 #include <bdvmi/xencontrol.h>
 
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
 	bdvmi::XenControl &ctrl = bdvmi::XenControl::Instance();
-	std::cout << "XEN version " << ctrl.runtime_version.first << "." << ctrl.runtime_version.second << std::endl;
+	std::cout << "XEN version " << ctrl.runtimeVersion.first << "." << ctrl.runtimeVersion.second << std::endl;
+	std::cout << "Unpausing domain " << std::stol(argv[1]) << std::endl;
+	ctrl.domainUnpause(std::stol(argv[1]));
 	return 0;
 }
