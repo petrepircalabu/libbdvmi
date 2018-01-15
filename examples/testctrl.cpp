ea46e05a0099768dc8967331fdbc1bf03f9c087e
Petre Pircalabu <ppircalabu@bitdefender.com>
2018-01-15 09:33:18 +0200

    SQUASHME: update testctrl to singleton
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

--- examples/testctrl.cpp
+++ examples/testctrl.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-	bdvmi::XenControl ctrl;
+	bdvmi::XenControl &ctrl = bdvmi::XenControl::Instance();
 	std::cout << "XEN version " << ctrl.runtime_version.first << "." << ctrl.runtime_version.second << std::endl;
 	return 0;
 }
