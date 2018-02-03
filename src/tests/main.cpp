544eb133fe1417c262f9b846a437decbf594c825
Petre Pircalabu <petre.pircalabu@gmail.com>
2018-02-03 06:50:07 +0200

    SQUASHME: Add catch2 unittest framework
    Signed-off-by: Petre Pircalabu <petre.pircalabu@gmail.com>

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_DEFAULT_REPORTER "automake"
+#include "catch.hpp"
+#include "catch_reporter_automake.hpp"
 
-int main( int argc, char *argv[] )
-{
-	std::cout << "Hello world!!!" << std::endl;
-	return 0;
+unsigned int Factorial( unsigned int number ) {
+  return number > 1 ? Factorial(number-1)*number : 1;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(0) == 1 );
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
 }
